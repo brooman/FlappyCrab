41db04b3966e58d940ae7e672fb34346503f0064
laykith <laykith@gmail.com>
2019-02-26 11:32:01 +0100

    Added a unit test

---
+++ Game/Game/Tests/NUnitTestClass.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using Game.Classes;
+
+namespace Game.Tests
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test()]
+        public void GameHasAPlayer()
+        {
+            Game game = new Game();
+
+            Assert.IsTrue(game.player is Player);
+        }
+    }
+}
+
+//Assert.IsTrue(Game.pillars.Count() >= 2);
+//Assert.IsTrue(Game.pillars.Count() % 2 == 0);
